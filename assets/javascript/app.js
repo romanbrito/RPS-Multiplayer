08106aded9a8bd34785aafe6782e0a6e720ca2b6
Roman Brito <Roman Brito>
2017-03-09 16:25:41 -0600

    on value initial state

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,30 @@ function player(name, attack) {
 var player1 = new player(null, null);
 var player2 = new player(null, null);
 
+// At initial load, get a snapshot of the current data
+
+database.ref().on("value", function (snapshot) {
+  if (snapshot.child("player1").exists()) {
+    // initial values
+    player1 = snapshot.val().player1;
+    $("#player1 p").html("player 1 name: " + player1.name);
+    renderButtons(1);
+  } else {
+
+  }
+  // if player 2 exists in firebase
+  if (snapshot.child("player2").exists()) {
+    player2 = snapshot.val().player2;
+    $("#player2 p").html("player 2 name: " + player2.name);
+    renderButtons(2);
+  } else {
+
+  }
+  // If any errors are experienced, log them to console.
+}, function(errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
 $("#submit-name").on("click", function (event) {
     // Prevent form from submitting
     event.preventDefault();
@@ -34,4 +58,8 @@ $(document).on("click", ".attackOptions", function () {
     } else if (dataPlayer === "playerNum2") {
       player2.attack = attackPlayer;
     }
+    database.ref().set({
+        player1,
+        player2
+    });
 });
